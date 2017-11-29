769a44a49c0ec90ef39d01775df54906cdb688fb
marprin <marprin93@gmail.com>
2017-11-29 18:18:21 +0700

    update resolver and fix mongodb url

--- mongo-connector.js
+++ mongo-connector.js
@@ -1,6 +1,6 @@
 const {MongoClient} = require('mongodb');
 
-const MONGO_URL = 'mongodb//localhost:27017/hackernews';
+const MONGO_URL = 'mongodb://localhost:27017/hackernews';
 
 module.exports = async () => {
     const db = await MongoClient.connect(MONGO_URL);
